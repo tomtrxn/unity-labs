1b4712506f93d38c32b355f7a65d4229a594e814
Tom <tomtrann3005@gmail.com>
2024-09-01 19:26:45 +1000

    added blue prefab instant. and fps setting

--- Assets/LoadAssets.cs
+++ Assets/LoadAssets.cs
@@ -6,11 +6,18 @@ using UnityEngine;
 public class LoadAssets : MonoBehaviour
 {
     public GameObject redObj;
+    [SerializeField] private GameObject blueObj; // This can be private with SerializeField to expose in the inspector
 
-    // Start is called before the first frame update
     void Start()
     {
-       Instantiate(redObj, Vector3.zero, Quaternion.identity);
+        Application.targetFrameRate = 60;
+
+
+        // Instantiate redObj at (0, 0, 0) with no rotation
+        Instantiate(redObj, Vector3.zero, Quaternion.identity);
+
+        // Instantiate blueObj at (-2, 0, 0) with no rotation
+        Instantiate(blueObj, new Vector3(-2, 0, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
